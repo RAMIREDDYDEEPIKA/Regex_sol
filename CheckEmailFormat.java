872da4bdf759a02446ee1fb922d8b1f92805e3ee
RAMIREDDYDEEPIKA <ramireddydeepika@gmail.com>
2024-12-20 15:07:08 +0530

    passing multiple input data using CsvSource

--- CheckEmailFormat.java
+++ CheckEmailFormat.java
@@ -1,18 +1,17 @@
 package sampleMavenProject.regexSol;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CheckEmailFormat {
 
     public boolean isValidEmail(String email) {
-        String regex = "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-z]{2,}$";
+        String regex = "^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\\.[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
@@ -20,24 +19,13 @@ public class CheckEmailFormat {
 
     @ParameterizedTest
     @CsvSource({
-            "hari@bridge.com",
-            "hari.sri@gmail.co",
-            "prana_kolagani@community.org",
-            "ashu.gmail@gmail.com"})
-    public void testValidEmails(String email) {
-        assertTrue(isValidEmail(email), "Expected email to be valid: " + email);
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "hari-email",
-            "Sru67@.com",
-            "@hari26.com",
-            "usha@bridge,com",
-            "ashu@gmail..com",
-            "harika@gmail"
+            "hari@bridge.com, true",
+            "hari.sri@gmail.co, true",
+            "prana_kolagani@community.org, true",
+            "ashu.gmail@gmail.com,true",
+            "hari34.com,true"
     })
-    public void testInvalidEmails(String email) {
-        assertFalse(isValidEmail(email), "Expected email to be invalid, but it passed validation: " + email);
+    public void testValidEmails(String email,boolean expected) {
+        assertEquals(expected,isValidEmail(email));
     }
 }
